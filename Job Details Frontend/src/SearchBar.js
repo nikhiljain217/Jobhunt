70d02fe0b9b70ed7f17b83235c3a9f13cc8f017f
Harshit Hajela <harshit.hajela@gmail.com>
2020-04-16 03:40:20 -0400

    refactor search bar css, add and style search button

--- Job Details Frontend/src/SearchBar.js
+++ Job Details Frontend/src/SearchBar.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import './SearchBar.css';
+import {FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
 function SearchBar() {
 
+    const displayResults = () => {
+        console.log("This would navigate to the job results page with the results fetched");
+    }
+
     return (
         <div className="search-bar">
-            <div className="search-control">
-                <input type="text" id="search-string"></input>
-                <input type="text" id="search-location"></input>
+            <div className="search-control" onSubmit={displayResults}>
+                <input type="text" id="search-string" placeholder="Search"></input>
+                <input type="text" id="search-location" placeholder="Location (state/zip code)"></input>
+                <Link to='/jobs'>
+                    <div className="search-icon"><FaSearch size="38px"/></div>
+                </Link>
             </div>
         </div>
     );
