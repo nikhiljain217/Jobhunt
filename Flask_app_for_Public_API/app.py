61f715087952dfbdafb66e7cef524d8d7c67df14
= <=>
2020-04-26 04:19:34 -0600

    fixed bug in elstic_search_insert and moved api to app.py

--- Flask_app_for_Public_API/app.py
+++ Flask_app_for_Public_API/app.py
@@ -117,7 +117,27 @@ def getUAValues():
         uaDict[item['name']]=item['href']
     
 
+@app.route('/covid/<city>/<state>')
+def covid_case(city, state):
 
+    try:
+        print("Got requests")
+        e = Elasticsearch()
+        res = e.search(index="cities_to_county",body={"query":{"bool":{"must":[{"match":{"city":city}},{"match":{"state":state}}]}}})
+        county = res['hits']['hits'][0]['_source']['county']
+
+        res = e.search(index="covid_19",body={"query":{"bool":{"must":[{"match":{"county":county}},{"match":{"state":state}}]}}})
+        print(res)
+        result = res['hits']['hits'][0]['_source']
+    except Exception as e:
+
+        print("The Covid Api have exception ")
+        traceback.print_exc()
+        result = {"county":"",   "state":"",   "dates":[], "cases":[], "deaths":[], "total_cases":0, "total_deaths":0}
+
+    return Response(response=jsonpickle.encode(result), status=200, mimetype="application/json")
+
+    
 if __name__ == '__main__':
     app.debug = True
     getUAValues()
