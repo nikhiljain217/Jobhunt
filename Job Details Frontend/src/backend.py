55aad5e7822f6819a09689b5760b2c633f0d44d8
Harshit Hajela <harshit.hajela@gmail.com>
2020-04-27 03:50:22 -0400

    sentiment analysis api

--- Job Details Frontend/src/backend.py
+++ Job Details Frontend/src/backend.py
@@ -4,12 +4,14 @@ from flask_cors import CORS
 from urllib.parse import quote
 from pprint import pprint
 from datetime import datetime
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 import csv
 import tweepy
 import jsonpickle
 import requests
 import time
+import unicodedata
 
 from twitter_keys import *
 from adzuna_keys import *
@@ -157,6 +159,105 @@ def start_tweet_streaming(company):
 
     tweet_stream.filter(track=[company])
 
+
+total_positive = 0
+total_negative = 0
+total_neutral = 0
+total_processed = 0
+quit_flag = True
+
+
+def do_sentiment_analysis(analyzer, tweets):
+    global total_negative
+    global total_neutral
+    global total_positive
+    global total_processed
+
+    total_processed += len(tweets)
+
+    #run sentiment analysis and update totals
+    for tweet in tweets:
+
+        tweet_text = ""
+        if hasattr(tweet, 'retweeted_status') and hasattr(tweet.retweeted_status, 'extended_tweet'):
+            tweet_text = tweet.retweeted_status.extended_tweet['full_text']
+        elif hasattr(tweet, 'full_text'):
+            tweet_text = tweet.full_text
+        else:
+            tweet_text = tweet.text
+                
+        str_norm = unicodedata.normalize("NFKD", tweet_text)
+        scores = analyzer.polarity_scores(str_norm)
+        scores.pop('compound')
+
+        top_sentiment = sorted(scores, key=scores.get, reverse=True)[0]
+        if top_sentiment == 'pos':
+            total_positive += 1
+        elif top_sentiment == 'neg':
+            total_negative += 1
+        else:
+            total_neutral += 1
+
+
+@socketio.on('start', namespace='/sentiments')
+def sentiment_start_streaming(company):
+    global twitter_api
+    global total_negative
+    global total_neutral
+    global total_positive
+    global total_processed
+    global quit_flag
+
+    def chunks(lst,n):
+        for i in range(0, len(lst), n):
+            yield lst[i:i+n]
+
+    try:
+        #twitter allows max 180 calls in 15 minutes, so wait for 5 seconds b/w calls
+        while(True):
+
+            #search for tweets for this company
+            results = []
+            results = twitter_api.search(q=company, lang='en', count=100, tweet_mode="extended")
+
+            if len(results) == 0:
+                break
+
+            analyzer = SentimentIntensityAnalyzer()
+
+            #process tweets in chunks of 20
+            for chunk in chunks(results,20):
+                if quit_flag:
+                    return
+
+                do_sentiment_analysis(analyzer, chunk)
+
+                #construct and emit response
+                resp = { 'total_negative': total_negative, 'total_neutral': total_neutral, 'total_positive': total_positive, 'total_processed': total_processed }
+                print("sent response {0}".format(resp))
+                socketio.emit('sentiment', jsonpickle.encode(resp), namespace='/sentiments')
+
+                time.sleep(1)
+
+    except Exception as e:
+        print("Exception occurred {0}".format(e))
+
+
+@socketio.on('connect', namespace='/sentiments')
+def sentiment_disconnect():
+    global quit_flag
+        
+    #allow streaming to begin
+    quit_flag = False
+
+
+@socketio.on('disconnect', namespace='/sentiments')
+def sentiment_disconnect():
+    global quit_flag
+    #signal streaming api to stop
+    quit_flag = True
+
+
 """
 @socketio.on_error_default
 def default_error_handler(e):
@@ -164,14 +265,6 @@ def default_error_handler(e):
     print(request.event["message"])
     print(request.event["args"])
 """
-
-def get_sentiment_analysis(company):
-    #search for tweets for this company in the last 7 days
-
-    #run sentiment analysis model day by day
-
-    #results current day results back to caller
-    return
 
 
 @app.route('/gethobbies/<location>', methods=['GET'])
